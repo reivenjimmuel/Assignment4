d01133e7056f6ee33cacaf675bb4ed1b80261591
Reiven Cabate <reivenjimmuelfcabate@iskolarngbayan.pup.edu.ph>
2021-11-06 12:27:03 +0800

    Second commit for 3rd Program. I modified this program by combining the two functions (excpet the displayOutput) and did some changes.

--- 3rdProgram.py
+++ 3rdProgram.py
@@ -1,19 +1,17 @@
 def getUserInput():
-    """ Function for money and cost input """
+
+    """ Function for money and cost input and
+     for computing the maximum amount of apples
+     that you can buy and the amount of your change"""
+     
     money_ = float(input('Enter the amount of your money: '))
     cost_ = float(input('Enter the price of an apple: '))
-    return money_, cost_
-  
-def amountAppleAndChange(money_, cost_):
-    """ Function for computing the max number of apples
-    and the total amount of change"""
-    apple_ = int(money_/cost_)
+    apple_= int(money_/cost_)
     change_ = float(money_%cost_)
     return apple_, change_
 
 def displayOutput(apples_, change_):
     print(f'You can buy {apples_} apples and your change is {change_:.2f} pesos.')
 
-money, cost = getUserInput()
-apple, change = amountAppleAndChange(money, cost)
-displayOutput(apple, change)
+apples, change = getUserInput()
+displayOutput(apples, change)
